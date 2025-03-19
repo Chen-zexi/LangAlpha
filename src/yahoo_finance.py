9926aea243bbdb46aeb01667afe8df9297d219eb
Alan Chen <zc2610@nyu.edu>
2025-03-19 00:55:23 -0400

    fixed a conflict in environment.yml, updated readme, add api key check

--- src/yahoo_finance.py
+++ src/yahoo_finance.py
@@ -11,6 +11,15 @@ class yahoo_finance:
         self.consumer_key = os.getenv('YAHOO_CONSUMER_KEY')
         self.consumer_secret = os.getenv('YAHOO_CONSUMER_SECRET')
         self.app_id = os.getenv('YAHOO_APP_ID')
+        self.auth = OAuth1(self.consumer_key, self.consumer_secret)
+        if self.consumer_key is None or self.consumer_secret is None:
+            raise ValueError("YAHOO_CONSUMER_KEY or YAHOO_CONSUMER_SECRET is not set")
+        else:
+            print("YAHOO_CONSUMER_KEY and YAHOO_CONSUMER_SECRET are set")
+        if self.app_id is None:
+            raise ValueError("YAHOO_APP_ID is not set")
+        else:
+            print("YAHOO_APP_ID is set")
 
     def get_data(self, ticker, start_date, end_date, interval='1d'):
         ticker = Ticker(ticker)
